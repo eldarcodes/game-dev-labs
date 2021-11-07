91e41ee2be9ec79b43ef45d9cd0dff6944cb6058
mirzabekov00 <prog.eldar@gmail.com>
2021-11-07 14:01:12 +0200

    feat(lab1): resolve task 13.1

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace myapp
+namespace Lab1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double result = getCoords(3.4, 6.7, 1.3, 2.4);
+            Console.WriteLine(result);
+        }
+
+        public static double getCoords(double x1, double x2, double y1, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
     }
 }
---
+++ lab1/README.md
@@ -0,0 +1,5 @@
+# Lab 1
+
+## Variant number - 13
+
+<img src="https://i.ibb.co/d5wXLBJ/download.png" />
